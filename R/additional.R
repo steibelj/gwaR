202e36ada7d568d40b9741c3b38c1525c0b951c6
steibelj <steibelj@msu.edu>
2017-01-20 09:52:56 -0500

    fit Manhattan

--- R/additional.R
+++ R/additional.R
@@ -103,35 +103,43 @@ qqgplot = function(pvector, add = F, ...) {
 #' @param map a genetic map: a data.frame with SNP or marker names in rows, chromosome name (chr column) and position (pos column)
 #' @param threshold the threshold of significance 
 #' @param col a vector of strings with color names for painting SNP according to their chromosome
+#' @param chrom a vector of chromosomal names to be included in the graphic by default all chromosomes are included 
 #' @param ... additional graphical parameters used by plot 
 #' @return NULL. A graphic is be generated
 #'  @export
 
-manhattan_plot <- function(pvalues, map, threshold = 0.01, col = c("black", "red"), chrom = NULL, ...) {
-    if (!("chr" %in% colnames(map))) 
-        stop("chr should be a column of map")
-    if (sum(rownames(map) == names(pvalues)) < length(pvalues)) 
-        stop("make sure that the values are ordered, rownames of map and names of p-value vector should agree")
-    if ((max(pvalues) > 1) | (min(pvalues) < 0)) 
-        stop("pvalues should be in the range [0,1]")
-    if (sum(pvalues == 0) > 0) {
-        pvalues[pvalues == 0] <- min(pvalues[pvalues > 0])
-        warning("some p-values were exactly zero and they were replaced with the next smallest value")
-    }
-    if (!is.null(chrom)) {
-        idx <- as.character(map$chr) %in% as.character(chrom)
-        map <- map[idx, ]
-        pvalues <- pvalues[idx]
-    }
-    if (length(col) < 2) 
-        stop("two colors are needed in the color vector")
-    mps <- as.numeric(as.character(map$chr))
-    plot(-log(pvalues, 10), pch = 20, col = col[(mps%%2) + 1], ylab = "-log10-pvalue", xlab = "chr", ..., axes = F)
-    axis(2)
-    lns <- (by(map, map$chr, nrow))
-    axis(1, at = c(0, cumsum(lns)[-length(lns)]) + as.vector(lns/2), labels = names(lns))
-    box()
-    abline(h = -log(threshold, 10), lwd = 2, col = "green")
+manhattan_plot<-function (pvalues, map, threshold = 0.01, col = c("black", "red"), 
+                          chrom = NULL, ...) 
+{
+  if (!("chr" %in% colnames(map))) 
+    stop("chr should be a column of map")
+  if (sum(rownames(map) == names(pvalues)) < length(pvalues)) 
+    stop("make sure that the values are ordered, rownames of map and names of p-value vector should agree")
+  if ((max(pvalues) > 1) | (min(pvalues) < 0)) 
+    stop("pvalues should be in the range [0,1]")
+  if (sum(pvalues == 0) > 0) {
+    pvalues[pvalues == 0] <- min(pvalues[pvalues > 0])
+    warning("some p-values were exactly zero and they were replaced with the next smallest value")
+  }
+  if (!is.null(chrom)) {
+    idx <- as.character(map$chr) %in% as.character(chrom)
+    map <- map[idx, ]
+    pvalues <- pvalues[idx]
+  }
+  if (length(col) < 2) 
+    stop("two colors are needed in the color vector")
+  lv<-as.character(unique(map$chr))
+  cl<-col[(1:length(lv))%%2+1]
+  names(cl)<-lv
+  plot(-log(pvalues, 10), pch = 20, col = cl[as.character(map$chr)], 
+       ylab = "-log10-pvalue", xlab = "chr", ..., axes = F)
+  axis(2)
+  lns <- (by(map, map$chr, nrow))
+  lns<-lns[lv]
+  axis(1, at = c(0, cumsum(lns)[-length(lns)]) + as.vector(lns/2), 
+       labels = lv)
+  box()
+  abline(h = -log(threshold, 10), lwd = 2, col = "green")
 }
 
 #'  Computes pvalues from a GWA class object represents them graphically. using \code{\link{manhattan_plot}} and \code{\link{qqgplot}}
